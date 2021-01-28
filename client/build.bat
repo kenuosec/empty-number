74c44864aec09b0bdc4638993b8d4a9f16d5c9e5
wuyigq <wuyigq@gmail.com>
2021-01-29 00:32:30 +0800

    -更新token

--- client/build.bat
+++ client/build.bat
@@ -1,5 +1,5 @@
 cd /d %~dp0
-pyinstaller -p D:/github/empty-number/client/venv/Lib/site-packages -D main.py --noconsole
+pyinstaller -p D:/github/empty-number/client/venv/Lib/site-packages -D main.py --noconsole -i "D:\github\empty-number\client\app.ico"
 xcopy main.ui .\dist\main\
 xcopy active.ui .\dist\main\
 xcopy add .\dist\ /s /e /h
